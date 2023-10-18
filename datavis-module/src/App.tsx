20ed21536877daa608c077b34ff398bfabffe2a1
Julian George <juliancgeorge@gmail.com>
2023-10-17 22:54:55 -0400

    cutting react template fat

--- datavis-module/src/App.tsx
+++ datavis-module/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { csv } from "d3";
 
 function App() {
+  const [data, setData] = useState<any[] | null>(null);
+  useEffect(() => {
+    csv(
+      "https://raw.githubusercontent.com/dali-lab/dali-challenges/main/data/Dartmouth%20-%20Courses.csv"
+    ).then((res) => {
+      setData(res);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
