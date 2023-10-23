d4f866f913e069284300025ff84d6f6846dc6fcc
Julian George <juliancgeorge@gmail.com>
2023-10-23 18:25:56 -0400

    Adding README, finishing profit bar graph & cleaning up code

---
+++ datavis-module/README.md
@@ -0,0 +1,15 @@
+## DALI Data Visualization Challenge
+
+### Julian George, 24W
+
+## Summary
+
+Using d3 (in several kinds of implementations), I visualized some data from the "Super Store" Dataset
+
+## Usage
+
+To use:
+
+1. Type `yarn` in terminal to install packages
+2. Type `yarn start` to start the web server
+3. See the graphs at `localhost:3000`!
--- datavis-module/src/components/CategoryProfitBar.tsx
+++ datavis-module/src/components/CategoryProfitBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback, RefObject } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import * as d3 from "d3";
 import "../App.css";
 
@@ -19,37 +19,88 @@ type TSubcategoryData = {
   [category: string]: { [subcategory: string]: TCategoryDatum };
 };
 
-type CategoryLists = {
+type TCategoryLists = {
   [category: string]: number[];
 };
 
-type SubcategoryLists = {
+type TSubcategoryLists = {
   [category: string]: {
     [subcategory: string]: number[];
   };
 };
 
-type BarDatum = {
+type TBarDatum = {
   category: string;
   mean: number;
 };
 
+type TRenderData = {
+  categoryList: string[];
+  xScaler: d3.ScaleBand<string>;
+  yScaler: d3.ScaleLinear<number, number, never>;
+  graphMax: number;
+  graphMin: number;
+  minMean: number;
+  maxMean: number;
+};
+
+const GRAPH_MARGIN_LEFT = 108;
+const GRAPH_MARGIN_BOTTOM = 32;
+const GRAPH_MARGIN_TOP = 0;
+const LABEL_BAR_PADDING = 8;
+
 const CategoryProfitBar = (props: ICategoryProfitBarProps) => {
   const { data, height, width } = props;
   const [categoryData, setCategoryData] = useState<TCategoryData>({});
+  const [renderData, setRenderData] = useState<TRenderData | null>(null);
   const [subcategoryData, setSubcategoryData] = useState<TSubcategoryData>({});
-  const [selectedData, setSelectedData] = useState<BarDatum[]>([]);
+  const [selectedData, setSelectedData] = useState<TBarDatum[]>([]);
   const [currentCategory, setCategory] = useState<string | null>(null);
+
   const svgRef = useRef<SVGSVGElement | null>(null);
+
+  const changeSelectedData = useCallback(
+    (category?: string) => {
+      const selectedCategories = category
+        ? subcategoryData[category]
+        : categoryData;
+      const newSelectedData = Object.entries(selectedCategories).map(
+        ([category, datum]) => ({ category: category, mean: datum.mean })
+      );
+      setSelectedData(newSelectedData);
+      const svgElement = d3.select(svgRef.current);
+      if (category) {
+        svgElement
+          .append("text")
+          .attr("x", GRAPH_MARGIN_LEFT)
+          .attr("y", height - 16)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .attr("class", "back-text")
+          .style("cursor", "pointer")
+          .style("font-weight", "bold")
+          .style("font-size", "10px")
+          .text("← Back")
+          .on("click", () => {
+            changeSelectedData();
+          });
+      } else {
+        svgElement.selectAll(".back-text").remove();
+      }
+      setCategory(category || null);
+    },
+    [subcategoryData, categoryData, setSelectedData, svgRef, height]
+  );
+
   useEffect(() => {
-    const categoryProfitLists: CategoryLists = {};
-    const subcategoryProfitLists: SubcategoryLists = {};
+    const categoryProfitLists: TCategoryLists = {};
+    const subcategoryProfitLists: TSubcategoryLists = {};
     // First, we go through the data, row by row,
     //   and add each datum's profit to a list corresponding to that datum's category and subcategory
     for (const row of data) {
       const category = row["Category"];
       // For our purposes, we just discard rows with no given category
-      if (category == "") continue;
+      if (category === "") continue;
       const subcategory = row["Sub-Category"];
       if (!(category in categoryProfitLists)) {
         categoryProfitLists[category] = [];
@@ -59,7 +110,7 @@ const CategoryProfitBar = (props: ICategoryProfitBarProps) => {
       categoryProfitLists[category].push(profit);
 
       // Skip the subcategory processing if it's blank
-      if (subcategory == "") continue;
+      if (subcategory === "") continue;
       if (!(subcategory in subcategoryProfitLists[category])) {
         subcategoryProfitLists[category][subcategory] = [];
       }
@@ -102,113 +153,83 @@ const CategoryProfitBar = (props: ICategoryProfitBarProps) => {
     changeSelectedData();
   }, [data]);
   // Updates the selectedData to either the subcategories of category, or the categories if the input is null
-  const changeSelectedData = useCallback(
-    (category?: string) => {
-      const selectedCategories = category
-        ? subcategoryData[category]
-        : categoryData;
-      const newSelectedData = Object.entries(selectedCategories).map(
-        ([category, datum]) => ({ category: category, mean: datum.mean })
-      );
-      setSelectedData(newSelectedData);
-      const svgElement = d3.select(svgRef.current);
-      if (category) {
-        svgElement
-          .append("text")
-          .attr("x", marginLeft)
-          .attr("y", height - 16)
-          .attr("fill", "currentColor")
-          .attr("text-anchor", "start")
-          .attr("class", "back-text")
-          .style("cursor", "pointer")
-          .style("font-weight", "bold")
-          .style("font-size", "10px")
-          .text("← Back")
-          .on("click", () => {
-            changeSelectedData();
-          });
-      } else {
-        svgElement.selectAll(".back-text").remove();
-      }
-      setCategory(category || null);
-    },
-    [subcategoryData, categoryData, setSelectedData, svgRef]
-  );
 
   useEffect(() => {
-    console.log("run");
-    const svgElement = d3.select(svgRef.current);
-    const bars = svgElement
-      .selectAll("rect")
-      .on("mouseover", (bar) => {
-        // d3.select(this).transition();
-      })
-      .transition()
-      .duration(100)
-      .ease(d3.easeLinear)
-      .style("opacity", 1);
+    const categoryList = selectedData.map((datum) => datum.category);
+    const xScaler = d3
+      .scaleBand()
+      .domain(categoryList)
+      .range([GRAPH_MARGIN_LEFT, width])
+      .padding(0.1);
+    const means = Object.values(selectedData).map((datum) => datum.mean);
+    const maxMean = d3.max(means) || 0;
+    const graphMax = maxMean * 1.5;
+    const minMean = d3.min(means) || 0;
+    const graphMin = minMean >= 0 ? 0 : minMean * 1.5;
+    const yScaler = d3
+      .scaleLinear()
+      .domain([graphMin, graphMax])
+      .range([height - GRAPH_MARGIN_BOTTOM, GRAPH_MARGIN_TOP]);
+    setRenderData({
+      categoryList,
+      xScaler,
+      yScaler,
+      graphMax,
+      graphMin,
+      minMean,
+      maxMean,
+    });
+  }, [height, width, selectedData]);
 
-    // .attr("opacity", 0);
-  }, [selectedData, svgRef]);
-
-  const marginLeft = 108;
-  const marginBottom = 32;
-  const marginTop = 0;
-  const categoryList = selectedData.map((datum) => datum.category);
-  const x = d3
-    .scaleBand()
-    .domain(categoryList)
-    .range([marginLeft, width])
-    .padding(0.1);
-  const means = Object.values(selectedData).map((datum) => datum.mean);
-  const maxMean = d3.max(means) || 0;
-  const graphMax = maxMean * 1.5;
-  const minMean = d3.min(means) || 0;
-  const graphMin = minMean >= 0 ? 0 : minMean * 1.5;
-  const y = d3
-    .scaleLinear()
-    .domain([graphMin, graphMax])
-    .range([height - marginBottom, marginTop]);
-
-  // Using refs here may be hacky, but it's better than working directly w/ SVGs like this recommends:
+  // Using refs here may be hacky, but it's better (imo) than working directly w/ SVGs like this recommends:
   //   https://2019.wattenberger.com/blog/react-and-d3
   useEffect(() => {
+    if (!renderData) return;
     const svgElement = d3.select(svgRef.current);
     // Clears previously rendered axes
     svgElement.selectAll(".axis").remove();
+    // Rerender bottom axis
     svgElement
       .append("g")
-      .call(d3.axisBottom(x))
-      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(renderData.xScaler))
+      .attr("transform", `translate(0,${height - GRAPH_MARGIN_BOTTOM})`)
       .attr("class", "axis x-axis");
+    // Rerender left (vertical) axis
     svgElement
       .append("g")
-      .call(d3.axisLeft(y))
+      .call(d3.axisLeft(renderData.yScaler))
       .call((g) =>
         g
           .append("text")
-          .attr("x", -1 * marginLeft + 4)
-          .attr("y", y((graphMax + graphMin) / 2))
+          .attr("x", -1 * GRAPH_MARGIN_LEFT + 4)
+          .attr(
+            "y",
+            renderData.yScaler((renderData.graphMax + renderData.graphMin) / 2)
+          )
           .attr("fill", "currentColor")
           .attr("text-anchor", "start")
           .attr("font-size", 12)
           .text("Mean Profit ($)")
       )
-      .attr("transform", `translate(${marginLeft},0)`)
+      .attr("transform", `translate(${GRAPH_MARGIN_LEFT},0)`)
       .attr("class", "axis");
-    if (minMean < 0) {
+
+    // If there are negative means, display zero line
+    if (renderData.minMean < 0) {
       svgElement
         .append("line")
         .style("stroke", "black")
         .style("stroke-width", 1)
         .attr("class", "zero-line")
-        .attr("x1", marginLeft)
-        .attr("y1", y(0))
+        .attr("x1", GRAPH_MARGIN_LEFT)
+        .attr("y1", renderData.yScaler(0))
         .attr("x2", width)
-        .attr("y2", y(0));
+        .attr("y2", renderData.yScaler(0));
     } else {
       svgElement.selectAll(".zero-line").remove();
     }
+
+    // If there's no "currentCategory" (if we're displaying a subcategory), display the back button
     if (!currentCategory) {
       svgElement
         .selectAll(".x-axis .tick")
@@ -219,7 +240,13 @@ const CategoryProfitBar = (props: ICategoryProfitBarProps) => {
         changeSelectedData(categoryName);
       });
     }
-  }, [selectedData, svgRef]);
+    svgElement
+      .selectAll("rect")
+      .transition()
+      .duration(100)
+      .ease(d3.easeLinear)
+      .style("opacity", 1);
+  }, [svgRef, renderData, changeSelectedData, currentCategory, height, width]);
   return (
     <div style={{ width, height }}>
       <h2 style={{ textAlign: "center" }}>
@@ -227,18 +254,43 @@ const CategoryProfitBar = (props: ICategoryProfitBarProps) => {
       </h2>
       <svg width={width} height={height} ref={svgRef}>
         <g>
-          {selectedData.map((datum) => (
-            <rect
-              key={datum.category}
-              x={x(datum.category)}
-              y={Math.min(y(datum.mean), y(0))}
-              height={Math.abs(y(0) - y(datum.mean))}
-              width={x.bandwidth()}
-              // opacity={0}
-              style={{ opacity: 0 }}
-              fill="steelblue"
-            ></rect>
-          ))}
+          {renderData != null
+            ? selectedData.map((datum) => (
+                <g key={datum.category}>
+                  <rect
+                    x={renderData.xScaler(datum.category)}
+                    y={Math.min(
+                      renderData.yScaler(datum.mean),
+                      renderData.yScaler(0)
+                    )}
+                    height={Math.abs(
+                      renderData.yScaler(0) - renderData.yScaler(datum.mean)
+                    )}
+                    width={renderData.xScaler.bandwidth()}
+                    // opacity={0}
+                    style={{ opacity: 0 }}
+                    fill="steelblue"
+                  ></rect>
+                  <text
+                    x={
+                      (renderData.xScaler(datum.category) || 0) +
+                      renderData.xScaler.bandwidth() / 2
+                    }
+                    // the Math.sign ensures that the text is inside for negative bars
+                    y={
+                      renderData.yScaler(datum.mean) +
+                      (Math.sign(datum.mean) === 1
+                        ? 12 + LABEL_BAR_PADDING
+                        : -1 * LABEL_BAR_PADDING)
+                    }
+                    textAnchor="middle"
+                    fill="white"
+                  >
+                    {Math.round(datum.mean * 100) / 100}
+                  </text>
+                </g>
+              ))
+            : "Loading..."}
         </g>
       </svg>
     </div>
